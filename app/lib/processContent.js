d1fb4ec9d5904676533435244f5d3d505e4110de
ShripadJoshi <shripad.j@terracez.com>
2026-04-03 20:53:46 +0530

    process content

---
+++ app/lib/processContent.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import pdfParse from "pdf-parse";
+
+const prisma = new PrismaClient();
+
+export async function processContent(contentId, buffer) {
+    try{
+        
+    }
+    catch(error){ 
+
+    }
+}
