1745da3e1030b9b0cf9cafcc7adf01c38546bab4
ShripadJoshi <shripad.j@terracez.com>
2026-04-03 20:53:35 +0530

    Process pipeline

---
+++ app/api/content/upload/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import { processContent } from "@/lib/processContent";
+
+
+
+export async function POST(request) {
+
+    try {
+        const formData = await request.formData();
+        const projectId = formData.get("projectId");
+        const type = formData.get("type");
+        const title = formData.get("title");
+
+        let fileUrl = null;
+        let youtubeUrl = null;
+        let transcript = null;
+
+        if (type === "PDF") {
+            const file = formData.get("file");
+
+            if (!file) {
+                return NextResponse.json({ error: "File is required for PDF type" }, { status: 400 });
+            }
+            const bytes = await file.arrayBuffer();
+            const buffer = Buffer.from(bytes);
+
+            fileUrl = "temp";
+            const content = await prisma.content.create({
+                data: {
+                    title,
+                    type: "PDF",
+                    fileUrl,
+                    projectId,
+                    status: "processing",
+                },
+            });
+
+            // async processing
+            processContent(content.id, buffer);
+
+            return NextResponse.json({ success: true, content });
+        }
+
+    }
+    catch (error) {
+
+    }
+}
---
+++ prisma/migrations/20260403143719_added_status_field_in_content/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Content" ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'processing';
