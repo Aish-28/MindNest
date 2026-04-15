6f1534ece7bbeb6fa4890b3339a4e3f9cbcb4a6f
ShripadJoshi <shripad.j@terracez.com>
2026-04-15 11:38:38 +0530

    changes

--- app/lib/geminiConfig.js
+++ app/lib/geminiConfig.js
@@ -3,10 +3,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-export async function askGemini(prompt) {
-  const result = await model.generateContent(prompt);
-  return result.response.text();
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 // export async function askGemini(prompt) {
 //   const result = await model.generateContent(prompt);
